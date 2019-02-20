5636d80ff52c83b043933c32271febe17b8fd041
zjl <zjl@zjldeMacBook-Air.local>
2019-02-20 16:42:31 +0800

    接收参数

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(u"hello word")
+
+
+# ?a=9&b=9
+def add(request):
+    # 接收参数
+    a = request.GET.get('a', 0)  # 设置默认值
+    b = request.GET.get('b', 0)  # 设置默认值
+    return HttpResponse(str(int(a) + int(b)))
+
+
+# 使用 /3/4/
+def add2(request, a, b):
+    return HttpResponse(str(int(a) + int(b)))
